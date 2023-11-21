772b3b847b61498cf274b7f3bc63b19d42e5b96d
kdy9960 <146910663+kdy9960@users.noreply.github.com>
2023-11-21 20:25:48 +0900

    회원가입 API 생성 유저를 저장하기 위한 UserService생성

---
+++ src/main/java/com/kdy9960/todoparty/UserService.java
@@ -0,0 +1,8 @@
+package com.kdy9960.todoparty;
+
+import org.springframework.stereotype.Service;
+
+@Service // 서비스로 운용할것이이기 @Service 어노테이션 주입
+public class UserService {
+    public void signup(UserRequestDto userRequestDto) // 회원가입 메서드로 signup 으로 이름 정한후 그에 관해 자료를 저장할 공간을 선정
+}
