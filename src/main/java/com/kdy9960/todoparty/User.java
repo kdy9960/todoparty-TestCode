52c7d044ffdcfe06888a2eafdab6e0f86ef6ab17
kdy9960 <146910663+kdy9960@users.noreply.github.com>
2023-11-21 20:17:21 +0900

    회원가입 API 생성을 위한 도메인 만들기, Entity 설정

---
+++ src/main/java/com/kdy9960/todoparty/User.java
@@ -0,0 +1,25 @@
+package com.kdy9960.todoparty;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "users") // DB 에서 실제 사용 하는 경우가 있으니 Table 어노테이션 으로 적용
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 유저 ID값 적용
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+
+    }
+}
